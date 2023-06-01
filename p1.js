5675ee30214a1096531916c181e0cb2df7b3cef6
Utka2000 <utkarsh.yashraj@dymensions.io>
2023-06-01 09:44:32 +0530

    opertion

---
+++ p1.js
@@ -0,0 +1,10 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+let firstName="Utkarsh"
+let lastName=" Yashraj"
+let fullName=firstName+lastName
+console.log(fullName)
